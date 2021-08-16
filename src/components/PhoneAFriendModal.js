df83bf7c9b257eb26b0c2daf0add0b650f36816c
Rick Beyer <beyer.richard73@gmail.com>
2021-08-16 13:53:56 -0500

    Phone A Friend button now reflects chosen friend. Changing friend selection also changes button text.

--- src/components/PhoneAFriendModal.js
+++ src/components/PhoneAFriendModal.js
@@ -1,11 +1,12 @@
 import './PhoneAFriendModal.css';
 import phoneafriend from '../phoneafriend.png';
 import Friend from './Friend';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const PhoneAFriendModal = (props) => {
 
-    let [activeFriend, setActiveFriend] = useState(0);
+    const [activeFriend, setActiveFriend] = useState(0);
+    const [buttonText, setButtonText] = useState('Call');
 
     const listFriends = props.friends.map((e, idx) => <Friend activeFriend={activeFriend} changeFriend={changeFriend} key={idx} friendNum={idx} name={e.name} twitter_id={e.twitter_id} />);
 
@@ -19,12 +20,16 @@ const PhoneAFriendModal = (props) => {
         setActiveFriend(key);
     }
 
+    useEffect(() => {
+        setButtonText(`Call ${props.friends[activeFriend].name}`);
+    },[activeFriend]);
+
     return (
         <div className='phone-a-friend-modal__container'>
             <div className='phone-a-friend-modal__inner'>
                 <img className='lifeline-image' src={phoneafriend} alt="modal__image" />
                 <div className='friend-container'>{listFriends}</div>
-                <div className='btn-call-name' onClick={btnCall}>Call</div>
+                <div className='btn-call-name' onClick={btnCall}>{buttonText}</div>
             </div>
         </div>
     )
