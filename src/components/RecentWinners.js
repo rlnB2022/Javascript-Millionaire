22931d20612707d549aa944ea5aabcd82cf12078
Rick Beyer <beyer.richard73@gmail.com>
2021-09-14 22:26:49 -0500

    Now shows No Recent Winners when no winners at all.

--- src/components/RecentWinners.js
+++ src/components/RecentWinners.js
@@ -1,13 +1,22 @@
 import './RecentWinners.css';
+import { useState } from 'react';
+import { useEffect } from 'react/cjs/react.development';
 
 const RecentWinners = (props) => {
 
+    const [numWinners, setNumWinners] = useState(0);
+
     const listItems = props.winners.map((e, idx) => <li key={idx}><div>{ e.name }</div><div>{ e.date }</div></li>);
+
+    useEffect(() => {
+        setNumWinners(listItems.length)
+    }, []);
 
     return (
         <div className="winners">
             <h1>Recent Winners</h1>
             <ul>
+                {numWinners === 0 ? <li>No Recent Winners</li> : null}
                 {listItems}
             </ul>
             <div id="viewAllWinners"><p className='view-all-winners__button' onClick={props.showViewAllWinners}>View All</p></div>
